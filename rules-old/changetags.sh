f6978f828b66a36e9d8dbd675d16d4dccea61322
Alan Watson Forster <68709385+alanwatsonforster@users.noreply.github.com>
2025-04-18 17:40:17 -0600

    Fix 1B change tags.

--- rules-old/changetags.sh
+++ rules-old/changetags.sh
@@ -4,10 +4,12 @@ find . -name "*.tex" |
 grep -v project.tex |
 xargs sed 's/{'$1'-[^}]*}/\n&\n/g' | 
 grep '^{'$1'-[^}]*}$' | 
+sed 's/{//;s/}//;s:/:\n:g' | 
 sort -u >rules.changetags
 
 sed 's/{'$1'-[^}]*}/\n&\n/g' project.tex | 
 grep '^{'$1'-[^}]*}$' | 
+sed 's/{//;s/}//;s:/:\n:g' | 
 sort -u >project.changetags
 
 diff -y project.changetags rules.changetags
