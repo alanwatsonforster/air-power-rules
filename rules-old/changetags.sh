0a6878a844f870839b17f006615f550c6e062f1f
Alan Watson Forster <68709385+alanwatsonforster@users.noreply.github.com>
2025-04-18 16:56:05 -0600

    Diff the change tags.

--- rules-old/changetags.sh
+++ rules-old/changetags.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
 find . -name "*.tex" |
+grep -v project.tex |
 xargs sed 's/{'$1'-[^}]*}/\n&\n/g' | 
 grep '^{'$1'-[^}]*}$' | 
-sort -u
+sort -u >rules.changetags
+
+sed 's/{'$1'-[^}]*}/\n&\n/g' project.tex | 
+grep '^{'$1'-[^}]*}$' | 
+sort -u >project.changetags
+
+diff -y project.changetags rules.changetags
